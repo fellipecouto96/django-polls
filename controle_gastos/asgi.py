471363f14b434b625b893ca25249e50786551bee
Fellipe Couto <fellipe@vulpi.com.br>
2021-07-16 11:19:57 -0300

    Create a view request with time zone.

--- controle_gastos/asgi.py
+++ controle_gastos/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for learning_python project.
+ASGI config for controle_gastos project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_python.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controle_gastos.settings')
 
 application = get_asgi_application()
