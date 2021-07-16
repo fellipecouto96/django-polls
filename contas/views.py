33c8c63807dbebcbf28f9b42b47d2c7fb27b15bf
Fellipe Couto <fellipe@vulpi.com.br>
2021-07-16 16:01:05 -0300

    Create a First Template with Jinja.

--- contas/views.py
+++ contas/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 import datetime
 
 def home(request):
-    now = datetime.datetime.now()
-    #html = "<html><body>It is now %s.</body></html>" % now
-    return render(request, 'contas/home.html')
+    data = {}
+    data['now'] = datetime.datetime.now()
+    data['transacoes'] = ['t1', 't2', 't3']
+
+    return render(request, 'contas/home.html', data)
